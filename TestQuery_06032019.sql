dd49f80f9f65de8148ee414667b1e89a3f77a358
Taisa Kushner <taisa.kushner@colorado.edu>
2019-06-04 09:22:19 +0530

    testQuery update

---
+++ TestQuery_06032019.sql
@@ -0,0 +1,63 @@
+-- SELECT TOP 100 CreatedOn, LocalTime 
+-- FROM [Talklife_Data].[dbo].[Talklife_Answer];
+
+SELECT TOP 5 COUNT(AnswerCount) AS Answered,
+    CreatedBy, 
+    COUNT(CreatedBy) AS NumQs--count takes the number of rows that match
+FROM [Talklife_Data].[dbo].[Talklife_Question]
+WHERE AnswerCount > 0 AND IsDeleted = 0
+GROUP BY CreatedBy
+ORDER BY COUNT(CreatedBy) DESC;
+
+SELECT TOP 5 COUNT(AnswerCount) AS NotAnswered,
+    CreatedBy, 
+    COUNT(CreatedBy) AS NumQs--count takes the number of rows that match
+FROM [Talklife_Data].[dbo].[Talklife_Question]
+WHERE AnswerCount = 0 AND IsDeleted = 0
+GROUP BY CreatedBy
+ORDER BY COUNT(CreatedBy) DESC;
+
+
+-- DECLARE @QsAnwered INTEGER;
+
+SELECT QsAnswered = COUNT(*)
+FROM [Talklife_Data].[dbo].[Talklife_Question]
+WHERE CreatedBy = 24 AND AnswerCount > 0;
+
+SELECT QsNotAnswered = COUNT(*)
+FROM [Talklife_Data].[dbo].[Talklife_Question]
+WHERE CreatedBy = 24 AND AnswerCount = 0;
+
+
+
+-- select one table based on another
+-- SELECT TOP 100 * FROM [Talklife_Data].[dbo].[Talklife_Question] AS Qs;
+-- SELECT TOP 100 * FROM [Talklife_Data].[dbo].[Talklife_Answer] AS Ans;
+
+SELECT TOP 5 [Talklife_Data].[dbo].[Talklife_Answer].[Content],
+[Talklife_Data].[dbo].[Talklife_Answer].[CreatedBy], 
+[Talklife_Data].[dbo].[Talklife_Answer].[Title],
+[Talklife_Data].[dbo].[Talklife_Answer].[QuestionId]
+FROM [Talklife_Data].[dbo].[Talklife_Answer]
+INNER JOIN [Talklife_Data].[dbo].[Talklife_Question]
+ON [Talklife_Data].[dbo].[Talklife_Answer].[QuestionId] = [Talklife_Data].[dbo].[Talklife_Question].[Id];
+
+-- SELECT [Talklife_Data].[dbo].[Talklife_Answer].* [Talklife_Data].[dbo].[Talklife_Question].*
+-- FROM [Talklife_Data].[dbo].[Talklife_Answer], [Talklife_Data].[dbo].[Talklife_Question]
+--     INNER JOIN Content [Talklife_Data].[dbo].[Talklife_Answer]
+--         ON [Talklife_Data].[dbo].[Talklife_Answer].[QuestionId] = [Talklife_Data].[dbo].[Talklife_Question].[Id];
+
+
+-- match answers to questions
+SELECT TOP 50
+ans.Content AS Ans,
+qs.Content AS OrigQ,
+ans.QuestionId AS AnsID,
+qs.Id AS QiD,
+qs.CreatedBy as QCreated,
+ans.CreatedBy as ACreated
+FROM [Talklife_Data].[dbo].[Talklife_Answer] as ans
+INNER JOIN [Talklife_Data].[dbo].[Talklife_Question] as qs
+ON ans.QuestionId = qs.Id
+WHERE qs.CreatedBy = 3                  -- add this line to pull only one person's timeline
+ORDER BY ans.QuestionId ASC;
--- infoPull.py
+++ infoPull.py
@@ -13,14 +13,16 @@ from collections import namedtuple
 
 class talkLife:
     def __init__(self, headings_list, data_csv,base_name):
-        self = pd.read_csv(data_csv, header=None, names=None)
+        self = pd.read_csv(data_csv, header=None, names=headings_list)
         self=self.fillna(method='ffill',axis=0)
 
-
+        print(self)
         #bigData = conCat([CGM,CARB,INS])
 
         #save2csv(bigData,base_name+'_all')
 
+def pullTimeZones(dataFrame):
+    
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -31,13 +33,12 @@ if __name__ == '__main__':
     file_name = sys.argv[2]
     base_name = sys.argv[3]
 
-    with open(heading_file) as csvfile:
-        readHeading = csv.reader(csvfile)
+    with open(heading_file,'r') as csvfile:
         headings = []
         for item in csvfile:
-            headings.append(item)
-
+            headings.append(item.rstrip()) #rstrip removes the newline character
+        csvfile.close()
     print(headings)
 
 
-    Data = talkLife(heading_name,file_name,base_name)
+    Data = talkLife(headings,file_name,base_name)
