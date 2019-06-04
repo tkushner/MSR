e7c56782649d901f38aaa042f24957fce27396dd
Taisa Kushner <taisa.kushner@colorado.edu>
2019-06-04 09:34:12 +0530

    forgot to save

--- TestQuery_06032019.sql
+++ TestQuery_06032019.sql
@@ -55,9 +55,14 @@ qs.Content AS OrigQ,
 ans.QuestionId AS AnsID,
 qs.Id AS QiD,
 qs.CreatedBy as QCreated,
-ans.CreatedBy as ACreated
+ans.CreatedBy as ACreated,
+ans.CreatedOn as ACreatedOn,
+ans.LocalTime as ALocalTime,
+qs.CreatedOn as QCreatedOn,
+qs.LocalTime as QLocalTime
 FROM [Talklife_Data].[dbo].[Talklife_Answer] as ans
 INNER JOIN [Talklife_Data].[dbo].[Talklife_Question] as qs
 ON ans.QuestionId = qs.Id
 WHERE qs.CreatedBy = 3                  -- add this line to pull only one person's timeline
+AND qs.IsDeleted = 0 AND ans.IsDeleted = 0
 ORDER BY ans.QuestionId ASC;
