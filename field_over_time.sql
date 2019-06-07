8d93fd9c5d152841e22f68f5445fccd3a739b271
Taisa Kushner <taisa.kushner@colorado.edu>
2019-06-07 18:03:40 +0530

    update fot

--- field_over_time.sql
+++ field_over_time.sql
@@ -1,1 +1,9 @@
--- pull a 
+-- pull a field over time, sum by day, see what the values are
+
+SELECT NewMood as NewMood_dailyMean,
+    CONVERT(date,CreatedOn) as _date,
+    --LEFT(CreatedOn, 11) as _date, 
+    COUNT(NewMood) as NumPosts
+FROM [Talklife_Data].[dbo].[Talklife_Question]
+GROUP BY CONVERT(date,CreatedOn), NewMood
+ORDER BY CONVERT(date,CreatedOn) ASC;
