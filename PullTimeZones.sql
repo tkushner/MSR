39f37147df9d0c222818cb45d383d09dfc8e080c
Taisa Kushner <taisa.kushner@colorado.edu>
2019-06-11 09:46:26 +0530

    qa match and time zones

---
+++ PullTimeZones.sql
@@ -0,0 +1,19 @@
+-- pull difference in times OG post and reply
+SELECT
+ans.Content AS Ans,
+qs.Content AS OrigQ,
+ans.QuestionId AS AnsID,
+qs.Id AS QiD,
+qs.CreatedBy as QCreated,
+ans.CreatedBy as ACreated,
+ans.CreatedOn as ACreatedOn,
+ans.LocalTime as ALocalTime,
+qs.CreatedOn as QCreatedOn,
+qs.LocalTime as QLocalTime
+FROM [Talklife_Data].[dbo].[Talklife_Answer] as ans
+INNER JOIN [Talklife_Data].[dbo].[Talklife_Question] as qs
+ON ans.QuestionId = qs.Id
+WHERE ans.LocalTime IS NOT NULL
+AND qs.LocalTime IS NOT NULL              
+AND qs.IsDeleted = 0 AND ans.IsDeleted = 0
+ORDER BY ans.QuestionId ASC;
